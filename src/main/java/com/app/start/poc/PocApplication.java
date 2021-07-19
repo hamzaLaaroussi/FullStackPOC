346ed2a0d9a7522488f49670ed0c6059d18cf139
laaroussi <hl.laaroussi@gmail.com>
2021-07-20 01:35:51 +0200

    Poc Spring boot

---
+++ src/main/java/com/app/start/poc/PocApplication.java
@@ -0,0 +1,18 @@
+package com.app.start.poc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public PocApplication(){
+
+	}
+
+	public static void main(String[] args) {
+		//SpringApplication.run(PocApplication.class, args);
+		System.out.println("Hello World");
+	
+	}
+
+}
